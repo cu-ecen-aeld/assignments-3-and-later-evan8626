831742a7633706f09f0255c58f88eee965819711
Evan Osborn <evan8626@gmail.com>
2022-11-18 10:01:57 -0700

    committing writer.sh

---
+++ finder-app/writer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script for writer
+# Author: Evan Osborn
+
+writefile=$1
+writestr=$2
+
+if [ -z $# ] 
+then
+	echo "Parameter not specified"
+	exit 1
+elif [ -z $1 ]
+then 
+	echo "Parameter 1 is not set as a directory"
+	exit 1
+elif [ -z $2 ]
+then
+	echo "Write string not specified"
+	exit 1
+elif [  $# -eq 2 ]
+then
+	echo writestr > $filename
+	exit 0
+fi
