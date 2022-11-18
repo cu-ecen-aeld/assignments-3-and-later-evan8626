c52616f76e6069c847708839caa1e2250abaf4c8
Evan Osborn <evan8626@gmail.com>
2022-11-18 09:46:20 -0700

    commiting manual-linux.sh

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -61,80 +61,79 @@ then
 fi
 
 # TODO: Create necessary base directories
-mkdir ${OUTDIR}/rootfs && cd ${OUTDIR}/rootfs && \
-mkdir -p bin dev etc home lib lib64 proc sys sbin tmp usr usr/bin usr/lib usr/sbin var var/log && \
-sudo chown -R root:root *
+mkdir ${OUTDIR}/rootfs 
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sys sbin tmp usr home/conf usr/bin usr/lib usr/sbin var var/log
+#sudo chown -R root:root *
 
-cd "$OUTDIR"
+cd ${OUTDIR}
 if [ ! -d "${OUTDIR}/busybox" ]
 then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
-    make distclean
-    make defconfig
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE distclean
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
 echo "Making and installing busybox..."
-make defconfig
+#make defconfig
 sudo bash -c "PATH=$PATH make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE"
-sudo bash -c "PATH=$PATH make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE install"
-#sudo env "PATH=$PATH" make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs/ install
+sudo bash -c "PATH=$PATH make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE CONFIG_PREFIX=${OUTDIR}/rootfs  install"
+make -j4 CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE
+sudo cp busybox ${OUTDIR}/rootfs/bin
+cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
-cd ${OUTDIR}/rootfs
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
 SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
-sudo cp -a ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
-sudo cp -a ${SYSROOT}/lib64/libc-2.31.so ${OUTDIR}/rootfs/lib64
-sudo cp -a ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
-sudo cp -a ${SYSROOT}/lib64/libm-2.31.so ${OUTDIR}/rootfs/lib64
-sudo cp -a ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
-sudo cp -a ${SYSROOT}/lib64/libresolv-2.31.so ${OUTDIR}/rootfs/lib64
-sudo cp -a ${SYSROOT}/lib64/ld-2.31.so ${OUTDIR}/rootfs/lib64
-sudo cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
-#sudo cp -r ${SYSROOT}/lib/ .
-#sudo cp -r ${SYSROOT}/libs/lib64 .
-#sudo cp -L ${SYSROOT}/lib/ld=linux-aarch64.* ${OUTDIR}/rootfs/lib
-#sudo cp -L ${SYSROOT}/lib64/libm.so.* ${OUTDIR}/rootfs/lib64
-#sudo cp -L ${SYSROOT}/lib64/libresolv.so.* ${OUTDIR}/rootfs/lib64
-#sudo cp -L ${SYSROOT}/lib64/libc.so.* ${OUTDIR}/rootfs/lib64
+sudo cp -a ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
+sudo cp -a ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
+sudo cp -a ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
+sudo cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
+sudo cp -a ${SYSROOT}/lib64/libc-*.so ${OUTDIR}/rootfs/lib64/
+sudo cp -a ${SYSROOT}/lib64/libm-*.so ${OUTDIR}/rootfs/lib64/
+sudo cp -a ${SYSROOT}/lib64/libresolv-*.so ${OUTDIR}/rootfs/lib64/
+sudo cp -a ${SYSROOT}/lib64/ld-2.31.so ${OUTDIR}/rootfs/lib64/
+echo "done with lib dependencies"
+
 
 # TODO: Make device nodes
 sudo mknod -m 666 dev/null c 1 3
-sudo mknod -m 600 dev/null c 5 1
+sudo mknod -m 600 dev/console c 5 1
 
-# TODO: Clean and build the writer utility
-cd ${FINDER_APP_DIR}/
-make clean
+# TODO: Clean and build the writer utility 
+cd ${FINDER_APP_DIR}
+make clean 
 make CROSS_COMPILE=$CROSS_COMPILE all
+#cd ${OUTDIR}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-#sudo -s cp -a writer ${OUTDIR}/rootfs/home/
-#sudo -s cp -a finder.sh ${OUTDIR}/rootfs/home/
-#sudo -s cp -a finder-test.sh ${OUTDIR}/rootfs/home/
-#sudo -s cp -a autorun-qemu.sh ${OUTDIR}/rootfs/home/
-#sudo -s cp -ra ../conf ${OUTDIR}/rootfs/home/conf
-sudo cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home/
-sudo cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home/
-sudo cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home/
-sudo cp -r ${FINDER_APP_DIR}/../conf ${OUTDIR}/rootfs/home/
-sudo cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home/
+sudo -s cp -a ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home/
+sudo -s cp -a ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home/
+sudo -s cp -a ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home/
+sudo -s cp -a ${FINDER_APP_DIR}/Makefile ${OUTDIR}/rootfs/home/
+sudo cp ${FINDER_APP_DIR}/conf/username.txt "$OUTDIR"/rootfs/home/conf/
+sudo cp ${FINDER_APP_DIR}/conf/assignment.txt "$OUTDIR"/rootfs/home/conf/
+sudo -s cp -a ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home/
+
 
 # TODO: Chown the root directory
 cd ${OUTDIR}/rootfs
-sudo chown -R root:root *
+sudo chown -R root:root * #${OUTDIR}/rootfs
 
 # TODO: Create initramfs.cpio.gz
+echo "creating initramfs image"
+cd ${OUTDIR}/rootfs
 find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
-cd ${OUTDIR}
+cd ..
 gzip -f initramfs.cpio
-mkimage -A arm -O linux -T ramdisk -d initramfs.cpio.gz uRamdisk
+#sudo chown -R root:root initramfs.cpio.gz
